303c0b428da36708fa68c13176f13c083b34067f
Ciri Ryan <cirijaye@yahoo.com>
2022-06-28 06:56:22 -0700

    add selectSong favorite title onClick button

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,19 @@
 import React from 'react';
-import { selectSong } from '../actions';
+// import { selectSong } from '../actions';
+import SongList from './SongList';
 
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <div className='ui container grid'>
+       <div className='ui row'>
+        <div className='column eight wide'>
+          <SongList />
+        </div>
+       </div>
+      
+    </div>
+  );
+};
 
 export default App;
